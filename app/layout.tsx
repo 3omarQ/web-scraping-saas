93384ccd555bf17742b7a8aea6ec6167ba4508bc
3omarQ <omarkassar.202@gmail.com>
2025-07-03 11:20:26 +0100

    Integrate Clerk for auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,9 +26,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider afterSignOutUrl={"/sign-in"}>
+
+      <html lang="en">
+        
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
